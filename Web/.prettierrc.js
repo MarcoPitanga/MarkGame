d14a701cb7199708914baee605c124c536207d9d
Marco Aurélio <marco.hub21@gmail.com>
2022-07-19 02:58:25 -0300

    :tada: Projeto criado com ReactJs e Laravel

---
+++ Web/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 120,
+  endOfLine: 'auto'
+}
---
+++ Web/src/templates/Home/index.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div className="bg-slate-800 w-screen h-screen">
+      <h1 className="text-xl">Hello World!</h1>
+    </div>
+  )
+}
