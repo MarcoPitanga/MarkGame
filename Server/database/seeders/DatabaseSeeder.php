17bbf207e6a5992a021658fb426016863eb84093
Marco Aurélio <marco.hub21@gmail.com>
2022-07-19 04:03:33 -0300

    :rocket: usuario criado (model, controller, migration, factory, seed)

--- Server/database/seeders/DatabaseSeeder.php
+++ Server/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Usuario;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Usuario::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
