d726fe975b8ae8ee7bceb38132b20213088c2b21
Marco Aurélio <marco.hub21@gmail.com>
2022-08-03 17:13:28 -0300

    :bug: modifica o baseurl do axios

--- Web/src/services/api.js
+++ Web/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://127.0.0.1:8000/api/'
+const baseUrl = 'https://markgame.herokuapp.com/api/'
 
 export const api = axios.create({
   baseURL: baseUrl
